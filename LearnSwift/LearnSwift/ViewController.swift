9c44982290ca7b13705d84655375f723948553e6
pengkang <1039512009@qq.com>
2016-12-06 15:59:11 +0800

    initied a new project

---
+++ LearnSwift/LearnSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearnSwift
+//
+//  Created by pengkang on 16/12/6.
+//  Copyright © 2016年 pengkang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
